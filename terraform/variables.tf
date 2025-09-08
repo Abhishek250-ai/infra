83e62b145d0b1b3735d6a644b347bdd90d55472f
Abhishek250-ai <bhat.abhishek@hotmail.com>
2025-09-08 15:14:33 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,44 +1,51 @@
+########################################
+# AWS + VPC
+########################################
 variable "aws_region" {
-  type    = string
-  default = "us-east-1"
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnets_cidrs" {
-  type = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnets_cidrs" {
-  type = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
-variable "ecr_account_id" {
-  type = string
-  description = "ECR account id which hosts the images (548586340409)"
-}
-
-variable "patient_image" {
-  type        = string
-  description = "Full URI for patient image, e.g. 5485..../patient:latest"
-}
-
-variable "appointment_image" {
-  type        = string
-  description = "Full URI for appointment image, e.g. 5485..../appointment:latest"
-}
-
+########################################
+# ECS + Containers
+########################################
 variable "container_port" {
-  type    = number
-  default = 80
+  description = "Port for ECS containers"
+  type        = number
+  default     = 80
 }
 
 variable "desired_count" {
-  type    = number
-  default = 2
+  description = "Number of ECS tasks to run"
+  type        = number
+  default     = 1
+}
+
+variable "patient_image" {
+  description = "ECR image URI for patient service"
+  type        = string
+}
+
+variable "appointment_image" {
+  description = "ECR image URI for appointment service"
+  type        = string
 }
