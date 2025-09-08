46674c7c5309e3fd1382648992680b461ce41985
Abhishek250-ai <bhat.abhishek@hotmail.com>
2025-09-08 15:18:34 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,14 @@
 output "alb_dns_name" {
-  description = "ALB DNS name"
+  description = "DNS name of the ALB"
   value       = aws_lb.alb.dns_name
 }
 
 output "patient_target_group_arn" {
-  value = aws_lb_target_group.patient_tg.arn
+  description = "ARN of the Patient target group"
+  value       = aws_lb_target_group.patient.arn
 }
 
 output "appointment_target_group_arn" {
-  value = aws_lb_target_group.appointment_tg.arn
+  description = "ARN of the Appointment target group"
+  value       = aws_lb_target_group.appointment.arn
 }
